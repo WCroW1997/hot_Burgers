a077a24918478eab26fb5da609e1c34d2791fdf1
WCroW1997 <wcrow1997@gmail.com>
2022-03-15 20:12:37 +0200

    first commit

---
+++ src/sample-restaurants.js
@@ -0,0 +1,26 @@
+// Наши рестораны
+const restaurants = [
+  {
+    title: 'Hot Burger Тверская',
+    url: 'hot-burger-tverskaya',
+    id: 1
+  },
+
+  {
+    title: 'Hot Burger Маросейка',
+    url: 'hot-burger-maroseyka',
+    id: 2
+  },
+  {
+    title: 'Hot Burger Петровка',
+    url: 'hot-burger-petrovka',
+    id: 3
+  },
+  {
+    title: 'Hot Burger Пушкина',
+    url: 'hot-burger-pushkina',
+    id: 4
+  }
+];
+
+export default restaurants;
