156864f8e769e113d5cddfb7d68187fefd1cf9b2
WCroW1997 <wcrow1997@gmail.com>
2022-03-22 14:53:42 +0200

    add burger

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MenuAdmin from './MenuAdmin';
 import Order from './Order';
 import Header from './Header';
+import Burger from './Burger';
+
 import sampleBurgers from '../sample-burgers'
 
 class App extends React.Component {
@@ -20,13 +22,27 @@ class App extends React.Component {
         this.setState({burgers: sampleBurgers})
     }
 
+    addToOrder = (key) =>{
+        const order = {...this.state.order}
+        order[key] = order[key] + 1 || 1
+        this.setState({order})
+    }
+
 
     render(){
         return(
             <div className='burger-paradise'>
                 <div className='menu'>
                     <Header title="Hot Burgers"/>
-                    
+                    <ul className='burgers'>
+                        {Object.keys(this.state.burgers).map(key => {
+                            return <Burger key={key} 
+                            index={key}
+                            details={this.state.burgers[key]}
+                            addToOrder={this.addToOrder}
+                            />
+                        })}
+                    </ul>
                 </div>
                 <Order/>
                 <MenuAdmin addBurger={this.addBurger} loadSampleBurgers={this.loadSampleBurgers}/>
--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 class Burger extends React.Component {
+    handleClick= () =>{
+        this.props.addToOrder(this.props.index)
+    }
+
+
     render(){
+        const {image, name, price, desc, status} = this.props.details
+        const isAvailable = status === 'available'
         return(
-            <p>Burger Compnent</p>
+            <li className='menu-burger'>
+                <div className='image-container'>
+                    <img src={image} alt={name}/>
+                </div>
+
+                <div className='burger-details'>
+                    <h3 className='burger-name'>
+                        {name}
+                        <span className='price'>{price}</span>
+                    </h3>
+                    <p>{desc}</p>
+                    <button 
+                        className='buttonOrder' 
+                        disabled={!isAvailable }
+                        onClick={this.handleClick}
+                            >
+                            { isAvailable ? 'Заказать' : 'Временно нет'}
+                    </button>
+                </div> 
+            </li>
         )
     }
 
